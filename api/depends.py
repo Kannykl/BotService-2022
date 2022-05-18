2a92c818338f87f8142b5e468a764eaf2a7b6ee4
Kanny <kanny@mail.ru>
2022-05-18 20:22:39 +0300

    Реализовано получение текущего пользователя от сервиса авторизации

--- api/depends.py
+++ api/depends.py
@@ -1,6 +1,7 @@
 """Service dependencies"""
-
-from fastapi import Depends
+from fastapi import Depends, Request, status, HTTPException
+from fastapi.security import SecurityScopes
+from core.config import settings
 from services.vk.vk_service import VKService
 from services.vk.bots_creation.bots import CreateVkBotsService
 from services.vk.bots_creation.phone_stock import OnlineSimPhoneStockService, PhoneStockService
@@ -25,11 +26,27 @@ def get_vk_service(bot_service: CreateVkBotsService = Depends(get_vk_bot_service
     return vk_service
 
 
-async def get_current_user():
+async def get_current_user(security_scopes: SecurityScopes, request: Request):
     """Get current user."""
-    client = AsyncClient(base_url="http://localhost:8003/auth/")
+    client = AsyncClient(base_url=settings.BASE_AUTH_URL)
 
-    response = await client.get("get_current_user/")
-    user = response.json()
+    token = request.cookies["Token"]
 
-    return user
+    response = await client.get("get_current_user", headers={
+        "Authorization": f"Bearer {token}",
+        "security-scopes": security_scopes.scope_str
+    })
+
+    if response.status_code == status.HTTP_403_FORBIDDEN:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail="Not enough permissions or invalid token",
+        )
+    if response.status_code == status.HTTP_401_UNAUTHORIZED:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail="Could not validate credentials",
+        )
+
+    return response.json()
+
