652e8839d1865dbbda5551b4ad89054849a366e1
Kanny <oleg.akulinushkin.01@mail.ru>
2022-06-05 15:39:19 +0300

    Добавлено ограничение на принимаемые значения boost type. Это не просто строка, а только like или sub.

--- schemas/task.py
+++ schemas/task.py
@@ -2,6 +2,7 @@
 
 from pydantic import BaseModel, Field, EmailStr
 import uuid
+from typing import Literal
 
 
 class Task(BaseModel):
@@ -22,4 +23,4 @@ class BoostTaskIn(BaseModel):
     """Info for create boost task"""
     link: str
     count: int
-    boost_type: str
+    boost_type: Literal["like", "sub"]
